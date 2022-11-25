69d7647457a84a9b7369046db27287720abe6b77
irisgaudin <iris.gaudin99@gmail.com>
2022-11-25 14:39:23 +0100

    add api weather back+front

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors())
 
 app.get("/", async (req, res) => {
   try {
@@ -13,14 +15,18 @@ app.get("/", async (req, res) => {
     // const response2 = await fetch("https://www.affirmations.dev/");
     // const data2 = await response2.json();
     // res.json({ api1: data, api2: data2 });
+    const responseWeather = await fetch ("https://api.openweathermap.org/data/2.5/weather?lat=48.8566&lon=2.3522&appid=69df7b8d93250c75b0422f77ae3f0484&units=metric&lang=fr");
+    const responseWeather2 = await responseWeather.json()
+    res.json({apiWeather:responseWeather2})
   } catch (error) {
     res.status(400).json(error.message);
-  }
+  } 
 });
 
 app.listen(3000, () => {
   console.log("Server has started");
 });
+
 
 // const client_id = "def7c58bf1dc4c55bafe1c7c9e0f5331"; // Your client id
 // const client_secret = "9b8ef3c114684d9ab02a70350b778dfe"; // Your secret
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,12 +1,40 @@
 
 const fetchData = async () => {
   try {
+
     const response = await fetch("http://localhost:3000/");
     const data = await response.json();
-    console.log(data);
+    console.log("reponse --->", data);
+    document.getElementById("ville").innerHTML= data.apiWeather.name 
+    document.getElementById('temperature').innerHTML= Math.round(data.apiWeather.main.temp)+" °C"
+    //console.log(document.getElementById('weather_icon').innerHTML = data.apiWeather.weather[0].icon )
+
+
+  // conditions d'affichage des icones meteo en fonction du code de l'icone:
+  let icon = data.apiWeather.weather[0].icon 
+  if (icon === '01d') {
+    document.querySelector('img').src='http://openweathermap.org/img/wn/01d@2x.png'
+  } 
+  if (icon === '02d') {
+    document.querySelector('img').src='http://openweathermap.org/img/wn/02d@2x.png'
+  } 
+  if (icon === '03d'|| icon ==='04d') {
+    document.querySelector('img').src='http://openweathermap.org/img/wn/04d@2x.png'
+  } 
+  if (icon === '09d'|| icon ==='10d') {
+    document.querySelector('img').src='http://openweathermap.org/img/wn/09d@2x.png'
+  } 
+  if (icon == '11d') {
+    document.querySelector('img').src='http://openweathermap.org/img/wn/11d@2x.png'
+  } 
+
+  
   } catch (error) {
     console.log(error.message);
   }
-};
+}; 
 
-fetchData();
+
+fetchData(); 
+
+// faire des conditions if le code de l'icone s'affiche, then affiche l'image dans la balise html 
