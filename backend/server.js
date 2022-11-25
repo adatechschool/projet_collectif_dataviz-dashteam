ff941408abbc4a172e16523ec5715fddf9143c75
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2022-11-25 17:00:40 +0100

    add authOptions, application requests authorization

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const morgan = require("morgan");
+const request = require("request");
 
 const app = express();
-
 app.use(express.json());
 app.use(morgan("dev"));
 
+/* Client Credentials oAuth2 flow to authenticate against the Spotify Accounts. */
+
+var client_id = "def7c58bf1dc4c55bafe1c7c9e0f5331";
+var client_secret = "9b8ef3c114684d9ab02a70350b778dfe";
+
+// Application requests authorization
+const authOptions = {
+  url: "https://accounts.spotify.com/api/token",
+  headers: {
+    Authorization:
+      "Basic " +
+      Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  form: {
+    grant_type: "client_credentials",
+  },
+  json: true,
+};
+
+request.post(authOptions, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    // Use the access token to access the Spotify Web API
+    const token = body.access_token;
+    console.log(token);
+  }
+});
+
+/* ----------------------------------------------------------------------------- */
+
 app.get("/", async (req, res) => {
   try {
-    // const response = await fetch("https://api.chucknorris.io/jokes/random");
-    // const data = await response.json();
-    // const response2 = await fetch("https://www.affirmations.dev/");
-    // const data2 = await response2.json();
-    // res.json({ api1: data, api2: data2 });
+    res.json("hello");
   } catch (error) {
     res.status(400).json(error.message);
   }
@@ -22,6 +48,8 @@ app.listen(3000, () => {
   console.log("Server has started");
 });
 
-// const client_id = "def7c58bf1dc4c55bafe1c7c9e0f5331"; // Your client id
-// const client_secret = "9b8ef3c114684d9ab02a70350b778dfe"; // Your secret
-// const redirect_uri = "REDIRECT_URI"; // Your redirect uri
+// const response = await fetch("https://api.chucknorris.io/jokes/random");
+// const data = await response.json();
+// const response2 = await fetch("https://www.affirmations.dev/");
+// const data2 = await response2.json();
+// res.json({ api1: data, api2: data2 });
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,11 +1,17 @@
-const fetchData = async () => {
-  try {
-    const response = await fetch("http://localhost:3000/");
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+// import sunshine from "../img/test2.jpg";
 
-fetchData();
+// import * as url from "https://upload.wikimedia.org/wikipedia/commons/3/3f/JPEG_example_flower.jpg";
+
+document.getElementById("test").src = "img/test2.jpg";
+
+// const fetchData = async () => {
+//   try {
+//     const response = await fetch("http://localhost:3000/");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// fetchData();
