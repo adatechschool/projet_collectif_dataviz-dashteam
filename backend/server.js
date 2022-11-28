a82241ca550b6f6b3a89ad108ae1927896fc778d
irisgaudin <iris.gaudin99@gmail.com>
2022-11-28 17:31:47 +0100

    link back to front to html --> finir les boucles

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
-
 const app = express();
 app.use(cors())
 app.use(express.json());
@@ -10,14 +8,18 @@ app.use(morgan("dev"));
 
 app.get("/", async (req, res) => {
   try {
-    // const response = await fetch("https://api.chucknorris.io/jokes/random");
-    // const data = await response.json();
-    // const response2 = await fetch("https://www.affirmations.dev/");
-    // const data2 = await response2.json();
-    // res.json({ api1: data, api2: data2 });
-    const responseNews = await fetch ("https://newsapi.org/v2/everything?q=javascript&from=2022-10-25&sortBy=publishedAt&apiKey=548a9b5db8f04f29b16fc57f77b4e7f1")
 
-    const responseNews2 = await responseNews.json() // PAS TERMINÉ 
+  } catch (error) {
+    res.status(400).json(error.message);
+  } 
+});
+
+app.get("/news/:keyword", async (req, res) => {
+  try {
+    console.log(req.params)
+    let domain = 'techcrunch.com'
+    const responseNews = await fetch (`https://newsapi.org/v2/everything?q=${req.params.keyword}&sortBy=publishedAt&apiKey=548a9b5db8f04f29b16fc57f77b4e7f1&language=en&language=fr&domains=${domain}`)
+    const responseNews2 = await responseNews.json() 
     console.log(responseNews2)
     res.json({apiNews:responseNews2})
   } catch (error) {
@@ -25,9 +27,10 @@ app.get("/", async (req, res) => {
   } 
 });
 
+
 app.listen(3000, () => {
   console.log("Server has started");
-});
+}); 
 
 
 // const client_id = "def7c58bf1dc4c55bafe1c7c9e0f5331"; // Your client id
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,6 +4,7 @@ const fetchData = async () => {
     const response = await fetch("http://localhost:3000/");
     const data = await response.json();
     console.log(data);
+
   } catch (error) {
     console.log(error.message);
   }
@@ -11,7 +12,113 @@ const fetchData = async () => {
 
 fetchData();
 
+// faire un bouton TECH dans le html et quand on clic alors lancer la fonction TECH (qui ajoute TECH a la fin de l'url derriere news)
+
+  const fetchNewsTech = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/news/tech");
+      const data = await response.json();
+      console.log(data);
+      
+    //article 1:
+    document.getElementById("newsTitle1").innerHTML=data.apiNews.articles[0].title
+    document.getElementById("author1").innerHTML="Written by : " + data.apiNews.articles[0].author 
+    document.getElementById("date1").innerHTML="Published at : " + data.apiNews.articles[0].publishedAt
+    document.getElementById("newsDescription1").innerHTML=data.apiNews.articles[0].description
+    document.getElementById("link1").href=data.apiNews.articles[0].url
+    document.getElementById("newsImage1").src=data.apiNews.articles[0].urlToImage
+
+    //article 2: 
+    document.getElementById("newsTitle2").innerHTML=data.apiNews.articles[1].title
+    document.getElementById("author2").innerHTML="Written by : " + data.apiNews.articles[1].author 
+    document.getElementById("date2").innerHTML="Published at : " + data.apiNews.articles[1].publishedAt
+    document.getElementById("newsDescription2").innerHTML=data.apiNews.articles[1].description
+    document.getElementById("link2").href=data.apiNews.articles[1].url
+    document.getElementById("newsImage2").src=data.apiNews.articles[1].urlToImage
+
+    // article 3: 
+    document.getElementById("newsTitle3").innerHTML=data.apiNews.articles[2].title
+    document.getElementById("author3").innerHTML="Written by : " + data.apiNews.articles[2].author 
+    document.getElementById("date3").innerHTML="Published at : " + data.apiNews.articles[2].publishedAt
+    document.getElementById("newsDescription3").innerHTML=data.apiNews.articles[2].description
+    document.getElementById("link3").href=data.apiNews.articles[2].url
+    document.getElementById("newsImage3").src=data.apiNews.articles[2].urlToImage
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  fetchNewsTech()
+
+  const fetchNewsApp = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/news/app");
+      const data = await response.json();
+      console.log(data);
+      
+    //article 1:
+    document.getElementById("newsTitle1").innerHTML=data.apiNews.articles[0].title
+    document.getElementById("author1").innerHTML="Written by : " + data.apiNews.articles[0].author 
+    document.getElementById("date1").innerHTML="Published at : " + data.apiNews.articles[0].publishedAt
+    document.getElementById("newsDescription1").innerHTML=data.apiNews.articles[0].description
+    document.getElementById("link1").href=data.apiNews.articles[0].url
+    document.getElementById("newsImage1").src=data.apiNews.articles[0].urlToImage
+
+    //article 2: 
+    document.getElementById("newsTitle2").innerHTML=data.apiNews.articles[1].title
+    document.getElementById("author2").innerHTML="Written by : " + data.apiNews.articles[1].author 
+    document.getElementById("date2").innerHTML="Published at : " + data.apiNews.articles[1].publishedAt
+    document.getElementById("newsDescription2").innerHTML=data.apiNews.articles[1].description
+    document.getElementById("link2").href=data.apiNews.articles[1].url
+    document.getElementById("newsImage2").src=data.apiNews.articles[1].urlToImage
+
+    // article 3: 
+    document.getElementById("newsTitle3").innerHTML=data.apiNews.articles[2].title
+    document.getElementById("author3").innerHTML="Written by : " + data.apiNews.articles[2].author 
+    document.getElementById("date3").innerHTML="Published at : " + data.apiNews.articles[2].publishedAt
+    document.getElementById("newsDescription3").innerHTML=data.apiNews.articles[2].description
+    document.getElementById("link3").href=data.apiNews.articles[2].url
+    document.getElementById("newsImage3").src=data.apiNews.articles[2].urlToImage
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+fetchNewsApp()
 
 
+const fetchNewsJavascript = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/news/javascript");
+    const data = await response.json();
+    console.log(data);
+    
+  //article 1:
+  document.getElementById("newsTitle1").innerHTML=data.apiNews.articles[0].title
+  document.getElementById("author1").innerHTML="Written by : " + data.apiNews.articles[0].author 
+  document.getElementById("date1").innerHTML="Published at : " + data.apiNews.articles[0].publishedAt
+  document.getElementById("newsDescription1").innerHTML=data.apiNews.articles[0].description
+  document.getElementById("link1").href=data.apiNews.articles[0].url
+  document.getElementById("newsImage1").src=data.apiNews.articles[0].urlToImage
 
+  //article 2: 
+  document.getElementById("newsTitle2").innerHTML=data.apiNews.articles[1].title
+  document.getElementById("author2").innerHTML="Written by : " + data.apiNews.articles[1].author 
+  document.getElementById("date2").innerHTML="Published at : " + data.apiNews.articles[1].publishedAt
+  document.getElementById("newsDescription2").innerHTML=data.apiNews.articles[1].description
+  document.getElementById("link2").href=data.apiNews.articles[1].url
+  document.getElementById("newsImage2").src=data.apiNews.articles[1].urlToImage
 
+  // article 3: 
+  document.getElementById("newsTitle3").innerHTML=data.apiNews.articles[2].title
+  document.getElementById("author3").innerHTML="Written by : " + data.apiNews.articles[2].author 
+  document.getElementById("date3").innerHTML="Published at : " + data.apiNews.articles[2].publishedAt
+  document.getElementById("newsDescription3").innerHTML=data.apiNews.articles[2].description
+  document.getElementById("link3").href=data.apiNews.articles[2].url
+  document.getElementById("newsImage3").src=data.apiNews.articles[2].urlToImage
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+fetchNewsJavascript()
